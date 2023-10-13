a6267aaeaae8aae0f7a832e4c620816e999534e9
Drakonxd786 <amoghmanishv@gmail.com>
2023-10-14 03:47:53 +0530

    #3 Morse.ino

--- morse/morse.ino
+++ morse/morse.ino
@@ -1,5 +1,5 @@
 // Define the led pins
-int ledPin = 9;
+int ledPin = 13;
 int led = ledPin;
 
 // Start the Serial Monitor
@@ -221,109 +221,83 @@ char decodeMorse(String morseCode) {
     return 'b';
   } else if (morseCode == "-.-.") {
     return 'c';
-  }
-  else if (morseCode == "-..") {
+  } else if (morseCode == "-..") {
     return 'd';
-  }
-  else if (morseCode == ".") {
+  } else if (morseCode == ".") {
     return 'e';
-  }
-  else if (morseCode == "..-.") {
+  } else if (morseCode == "..-.") {
     return 'f';
-  }
-  else if (morseCode == "--."){
+  } else if (morseCode == "--.") {
     return 'g';
-  }
-  else if (morseCode == "....") {
+  } else if (morseCode == "....") {
     return 'h';
-  }
-  else if (morseCode == "..") {
+  } else if (morseCode == "..") {
     return 'i';
-  }
-  else if (morseCode == ".---") {
+  } else if (morseCode == ".---") {
     return 'j';
-  }
-  else if (morseCode == "-.-") {
+  } else if (morseCode == "-.-") {
     return 'k';
-  }
-  else if (morseCode == ".-..") {
+  } else if (morseCode == ".-..") {
     return 'l';
-  }
-  else if (morseCode == "--") {
+  } else if (morseCode == "--") {
     return 'm';
-  }
-  else if (morseCode == "-.") {
+  } else if (morseCode == "-.") {
     return 'n';
-  }
-  else if (morseCode == "---") {
+  } else if (morseCode == "---") {
     return 'o';
-  }
-  else if (morseCode == ".--.") {
-    return 'p';    
-  }
-  else if (morseCode == "--.-") {
-    return 'q';    
-  }
-  else if (morseCode == ".-.") {
+  } else if (morseCode == ".--.") {
+    return 'p';
+  } else if (morseCode == "--.-") {
+    return 'q';
+  } else if (morseCode == ".-.") {
     return 'r';
-  }
-  else if (morseCode == "...") {
+  } else if (morseCode == "...") {
     return 's';
-  }
-  else if (morseCode == "-") {
+  } else if (morseCode == "-") {
     return 't';
-  }
-  else if (morseCode == "..-") {
+  } else if (morseCode == "..-") {
     return 'u';
-  }
-  else if (morseCode == "...-") {
+  } else if (morseCode == "...-") {
     return 'v';
-  }
-  else if (morseCode == ".--") {
+  } else if (morseCode == ".--") {
     return 'w';
-  }
-  else if (morseCode == "-..-") {
+  } else if (morseCode == "-..-") {
     return 'x';
-  }
-  else if (morseCode == "-.--") {
+  } else if (morseCode == "-.--") {
     return 'y';
-  }
-  else if (morseCode == "--..") {
+  } else if (morseCode == "--..") {
     return 'z';
-  }
-  else if (morseCode == ".----") {
-    return '1';    
-  }
-  else if (morseCode == "..---") {
+  } else if (morseCode == ".----") {
+    return '1';
+  } else if (morseCode == "..---") {
     return '2';
-  }
-  else if (morseCode == "...--") {
+  } else if (morseCode == "...--") {
     return '3';
-  }
-  else if (morseCode == "....-") {
+  } else if (morseCode == "....-") {
     return '4';
-  }
-  else if (morseCode == ".....") {
+  } else if (morseCode == ".....") {
     return '5';
-  }
-  else if (morseCode == "-....") {
+  } else if (morseCode == "-....") {
     return '6';
-  }
-  else if (morseCode == "--...") {
+  } else if (morseCode == "--...") {
     return '7';
-  }
-  else if (morseCode == "---..") {
+  } else if (morseCode == "---..") {
     return '8';
-  }
-  else if (morseCode == "----.") {
+  } else if (morseCode == "----.") {
     return '9';
-  }
-  else if (morseCode == "-----") {
+  } else if (morseCode == "-----") {
     return '0';
-  }
-  else {
+  } else {
     return ' ';
   }
+}
+
+int stringLength(String str) {
+  int length = 0;
+  while (str[length] != '\0') {
+    length++;
+  }
+  return length;
 }
 
 void loop() {
@@ -331,53 +305,43 @@ void loop() {
   int len = 0;
   int len1 = 0;
   String binval;
-
-  int x;
-  Serial.println("Please enter 1 or 0 if you want to either convert a string into morse code or vice versa respectively:")
-  x = Serial.read();
-  if (x == 1) {
-  Serial.println("Please enter your string: ");
-  
+  Serial.println("Please enter 1 or 0 if you want to either convert a string into morse code or vice versa respectively:");
   while (Serial.available() == 0) {
   }
-  String input = Serial.readString();
-  input.trim();
-  Serial.read();
-  len = input.length();
-
-  Serial.println(input);
-  for (int i = 0; i < len; i++) {
-    n = input.charAt(i);
-    morse(n);
-  }
-  }
-  else if(x == 0) {
-  String morseCode;
-  if (Serial.available() >= 2) {
-    char key1 = Serial.read();
-    delay(10);
-    char key2 = Serial.read();
-
-    // Perform actions based on the input
-    if (key1 == 'G') {
-      return '.';
-    } 
-    else if (key2 == 'H') {
-      return '-';
+  int x = Serial.read();
+  if (x == 49) {
+    Serial.println("Please enter your string: ");
+    while (Serial.available() == 0) {
+    }
+    String input = Serial.readString();
+    input.trim();
+    Serial.read();
+    len = input.length();
+    Serial.println(input);
+    for (int i = 0; i < len; i++) {
+      n = input.charAt(i);
+      morse(n);
     }
   }
-  for (int i = 0; i < len; i++) {
-    if (input.charAt(i) != ' ') {
-      morseCode += input.charAt(i);
-    } else {
+  if (x == 48) {
+    String morseCode;
+    Serial.println("Enter morse code");
+    while (Serial.available() == 0) {
+    }
+    String input = Serial.readString();
+    len = stringLength(input);
+    for (int i = 0; i < len; i++) {
+      if (input.charAt(i) != ' ') {
+        morseCode += input.charAt(i);
+      } else {
+        char decodedChar = decodeMorse(morseCode);
+        Serial.print(decodedChar);
+        morseCode = "";
+      }
+    }
+    if (morseCode != "") {  //Last Character
       char decodedChar = decodeMorse(morseCode);
-      Serial.print(decodedChar);
-      morseCode = "";
+      Serial.println(decodedChar);
     }
-  }
-  if (morseCode != "") {   //Last Character 
-    char decodedChar = decodeMorse(morseCode);
-    Serial.print(decodedChar);
-  }
   }
 }
